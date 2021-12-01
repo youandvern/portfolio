f72764c6bd15e078cacd3ca9113e45bbc5d3f331
youandvern <ayounguw@gmail.com>
2021-11-30 21:58:21 -0800

    Finish refactor WordLink, WordGroup, and Step2

--- src/components/PageScheduler/step2.js
+++ src/components/PageScheduler/step2.js
@@ -1,141 +1,212 @@
-import React, { useState } from 'react';
-import {useEffect, useRef } from 'react';
-import {gsap} from 'gsap';
-import {getRndWave, getRndScale, getRndDuration, getRndAmount} from './waves.js'
-import WordGroup from '../WordGroup';
+import React from "react";
+import { useEffect, useRef, createRef } from "react";
+import { gsap } from "gsap";
+import { getRndWave, getRndScale, getRndDuration, getRndAmount } from "./waves.js";
+import WordGroup from "../WordGroup";
 
-export default function Step2(props) {
-
-  
+/**
+ * Menu component which is a collection of WordLinks as menu options. Currently cannot be updated dynamically.
+ * @param   {function(boolean)} onFinish  Function will be called with param=false when ready to unmount this component.
+ * @param   {function(string)} onNext  Function will be called with param=id when ready to show next component (Id).
+ * @param   {function({x: Float32Array, y: float})} setClickPos  Function will be called with param=clickLocation when a menu item is clicked.
+ * @param   {string} menu  Set to "menu0" if navigation should automatically select the first menu item.
+ * @param   {function(string)} setMenu  Will be called with param=false to reset autonavigation.
+ * @return  {html}
+ */
+export default function Step2({ onFinish, onNext, setClickPos, menu, setMenu }) {
+  // prepare data for wordlink menu items
+  // wordLinkDetails = [{id: string, text: string, reference: useRef(null), onClick: function no args}, ...]
   const menu_items = ["PROJECTS", "ABOUT", "CONTACT", "RESUME"];
-  const menu_refs = useRef([]);
+  const menu_refs = menu_items.map(() => createRef());
+  const wordLinkDetails = menu_items.map((text, index) => ({
+    id: "menu" + index,
+    text: text,
+    reference: menu_refs[index],
+    onClick: () => handleTextClick("menu" + index, menu_refs[index]),
+  }));
 
-  useEffect(() => {
-    menu_refs.current = menu_refs.current.slice(0, menu_items.length);
-  }, [menu_items]);
-
-  // const wordLinkDetails = [{id: {string}, text: {string} reference: {useRef}, onClick: {function no args}}]
-  const wordLinkDetails = menu_items.map((text, index) =>
-    ({
-      id: ("menu"+index),
-      text: text,
-      reference: menu_refs.current[index],
-      onClick: () => handleTextClick(this.id, this.reference )
-    })
-  );
-
-  const menu_id = useRef('menu0');
-
-  const scale_float = useRef(gsap.timeline({repeat: -1, paused: true}));
-  const xfloat = useRef(gsap.timeline({repeat: -1, paused: true}));
-  const yfloat = useRef(gsap.timeline({repeat: -1, paused: true}));
-  const first_click_tl = useRef(gsap.timeline({ paused: true}));
+  // create references for motion timelines
+  const scale_float = useRef(gsap.timeline({ repeat: -1, paused: true }));
+  const xfloat = useRef(gsap.timeline({ repeat: -1, paused: true }));
+  const yfloat = useRef(gsap.timeline({ repeat: -1, paused: true }));
+  const first_click_tl = useRef(gsap.timeline({ paused: true }));
 
   // set this step to false when exiting
-  function finishStep(){
-    props.onFinish(false);
+  function finishStep() {
+    onFinish(false);
   }
 
+  // show the WordGroup menu on render (scale from 0)
   function renderMotion() {
-    gsap.to(".wordlink-list", {scale:1, duration: 1, ease: "sine.inOut"});
-  };
+    gsap.to(".wordlink-list", { scale: 1, duration: 1, ease: "sine.inOut" });
+  }
 
+  // create steps to execute when component renders
   useEffect(() => {
+    // always show render motion
     renderMotion();
 
     // navigate directly to projects page if projects clicked from resume page, reset menu
-    if (props.menu == "menu0"){
-      props.setMenu(false);
-      setTimeout(() => { handleTextClick("menu0", menu_refs[0])}, 1000);
+    if (menu === "menu0") {
+      setMenu(false);
+      setTimeout(() => {
+        handleTextClick("menu0", menu_refs[0]);
+      }, 1000);
 
-      // begin wave motion when rendered
+      // begin wave motion when rendered if not going straight to projects page
     } else {
       startWaveMotion();
     }
   }, []);
 
-
-  function cleanTimelines(){
+  // kill timelines before exiting
+  function cleanTimelines() {
     scale_float.current.kill();
     xfloat.current.kill();
     yfloat.current.kill();
   }
 
+  // set wave motions in 3D for items in menu
   function startWaveMotion() {
+    // clear and set y-direction motions (text elements only)
     yfloat.current.clear();
-    let y_duration, y_d_sum = 0;
+    let y_duration,
+      y_d_sum = 0;
     for (let i = 0; i < 8; i++) {
       y_duration = getRndDuration();
-      yfloat.current.to(".link-text", {y: getRndWave(), ease: "sine.inOut", duration: y_duration, stagger: {from:"start", amount:getRndAmount()}}, y_d_sum);
+      yfloat.current.to(
+        ".link-text",
+        {
+          y: getRndWave(),
+          ease: "sine.inOut",
+          duration: y_duration,
+          stagger: { from: "start", amount: getRndAmount() },
+        },
+        y_d_sum
+      );
       y_d_sum += y_duration;
-    };
-    yfloat.current.to(".link-text", {y: 0, ease: "sine.inOut", duration: getRndDuration(), stagger: {from:"start", amount:getRndAmount()}}, y_d_sum);
+    }
+    yfloat.current.to(
+      ".link-text",
+      {
+        y: 0,
+        ease: "sine.inOut",
+        duration: getRndDuration(),
+        stagger: { from: "start", amount: getRndAmount() },
+      },
+      y_d_sum
+    );
 
+    // clear and set x-direction motions (container with text and shadow)
     xfloat.current.clear();
-    let x_duration, x_d_sum = 0;
+    let x_duration,
+      x_d_sum = 0;
     for (let i = 0; i < 9; i++) {
       x_duration = getRndDuration();
-      xfloat.current.to(".wordlink-container", {x: getRndWave(), ease: "sine.inOut", duration: x_duration, stagger: {from:"end", amount:getRndAmount()}}, x_d_sum);
+      xfloat.current.to(
+        ".wordlink-container",
+        {
+          x: getRndWave(),
+          ease: "sine.inOut",
+          duration: x_duration,
+          stagger: { from: "end", amount: getRndAmount() },
+        },
+        x_d_sum
+      );
       x_d_sum += x_duration;
-    };
-    xfloat.current.to(".wordlink-container", {x: 0, ease: "sine.inOut", duration: getRndDuration(), stagger: {from:"end", amount:getRndAmount()}}, x_d_sum);
+    }
+    xfloat.current.to(
+      ".wordlink-container",
+      {
+        x: 0,
+        ease: "sine.inOut",
+        duration: getRndDuration(),
+        stagger: { from: "end", amount: getRndAmount() },
+      },
+      x_d_sum
+    );
 
+    // clear and set z-direction (scale) motions (outer container)
     scale_float.current.clear();
-    let s_duration, s_d_sum = 0;
+    let s_duration,
+      s_d_sum = 0;
     for (let i = 0; i < 4; i++) {
       s_duration = getRndDuration();
-      scale_float.current.to(".wordlink-outer-container", {scale: getRndScale(), ease: "sine.inOut", duration: s_duration, stagger: {from:"center", amount:getRndAmount()}}, s_d_sum);
+      scale_float.current.to(
+        ".wordlink-outer-container",
+        {
+          scale: getRndScale(),
+          ease: "sine.inOut",
+          duration: s_duration,
+          stagger: { from: "center", amount: getRndAmount() },
+        },
+        s_d_sum
+      );
       s_d_sum += s_duration;
-    };
-    scale_float.current.to(".wordlink-outer-container", {scale: 1, ease: "sine.inOut", duration: getRndDuration(), stagger: {from:"center", amount:getRndAmount()}}, s_d_sum);
+    }
+    scale_float.current.to(
+      ".wordlink-outer-container",
+      {
+        scale: 1,
+        ease: "sine.inOut",
+        duration: getRndDuration(),
+        stagger: { from: "center", amount: getRndAmount() },
+      },
+      s_d_sum
+    );
 
-    scale_float.current.paused( false );
+    // play all 3 motion timelines once steps are defined
+    scale_float.current.paused(false);
     xfloat.current.paused(false);
     yfloat.current.paused(false);
   }
 
-  function handleContainerClick (reference) {
+  // handle click event on a menu item. Spin other items away from clicked item.
+  function handleTextClick(id, reference) {
+    // get position of clicked item from reference
     let click_position = reference.current.getBoundingClientRect();
-    props.setClickPos({x: (click_position.x + click_position.width / 2 ), y: click_position.y})
-  }
+    setClickPos({ x: click_position.x + click_position.width / 2, y: click_position.y });
 
-  function handleTextClick (id, reference) {
-    menu_id.current = id;
-
-    let click_position = reference.current.getBoundingClientRect();
-    props.setClickPos({x: (click_position.x + click_position.width / 2 ), y: click_position.y})
-
-
+    // create spinning motions for not-clicked menu items
     const idno = id.slice(4);
     const def_delay = 0.4;
     const words = gsap.utils.toArray(".wordlink-outer-container");
-    gsap.to(".text-shadow", {scale: 0, ease:"power3.in", duration: def_delay});
-    for (let i=0; i<words.length; i++) {
+    gsap.to(".text-shadow", { scale: 0, ease: "power3.in", duration: def_delay });
+    for (let i = 0; i < words.length; i++) {
       let x_trans = window.innerWidth;
-      let spin_dur = 0.9 * Math.abs(idno-i);
-      let tran_dur =  1.8 * Math.abs(idno-i);
-      let spin_rep = Math.ceil( 3 / spin_dur);
-      if (i < idno){
-        first_click_tl.current.to(words[i], {x: -x_trans, duration: tran_dur}, def_delay);
-        first_click_tl.current.to(words[i], {rotation: '-=360', duration: spin_dur, repeat:spin_rep}, def_delay);
+      let spin_dur = 0.9 * Math.abs(idno - i);
+      let tran_dur = 1.8 * Math.abs(idno - i);
+      let spin_rep = Math.ceil(3 / spin_dur);
+      if (i < idno) {
+        first_click_tl.current.to(words[i], { x: -x_trans, duration: tran_dur }, def_delay);
+        first_click_tl.current.to(
+          words[i],
+          { rotation: "-=360", duration: spin_dur, repeat: spin_rep },
+          def_delay
+        );
       } else if (i == idno) {
-        first_click_tl.current.to(words[i], {scale: 0, ease:"power3.in", duration:def_delay}, 0);
+        first_click_tl.current.to(
+          words[i],
+          { scale: 0, ease: "power3.in", duration: def_delay },
+          0
+        );
       } else {
-        first_click_tl.current.to(words[i], {x: x_trans, duration: tran_dur}, def_delay);
-        first_click_tl.current.to(words[i], {rotation: '+=360', duration: spin_dur, repeat:spin_rep}, def_delay);
+        first_click_tl.current.to(words[i], { x: x_trans, duration: tran_dur }, def_delay);
+        first_click_tl.current.to(
+          words[i],
+          { rotation: "+=360", duration: spin_dur, repeat: spin_rep },
+          def_delay
+        );
       }
     }
 
-    first_click_tl.current.paused( false );
+    // play click motion once steps defined
+    first_click_tl.current.paused(false);
 
-    setTimeout( () => props.onNext(menu_id.current), 400); // begin to show clicked page ( onNext != false )
-    setTimeout( () => finishStep(), 1000); // un render menu after a second ( allows full animation to play through )
+    setTimeout(() => onNext(id), 400); // begin to show clicked page ( onNext != false )
+    setTimeout(() => finishStep(), 1000); // un render menu after a second ( allows full animation to play through )
     cleanTimelines();
   }
 
-
-  return (
-    <WordGroup wordLinkDetails={wordLinkDetails} />
-  )
-
+  return <WordGroup wordLinkDetails={wordLinkDetails} />;
 }
--- src/components/WordGroup/index.jsx
+++ src/components/WordGroup/index.jsx
@@ -9,48 +9,19 @@ import WordLink from "../WordLink";
  * @param   {string} wordLinkDetails[].text  Display text for WordLink menu item.
  * @param   {useRef} wordLinkDetails[].reference  Unique useRef hook attached to WordLink menu item.
  * @param   {function} wordLinkDetails[].onClick  No-argument function called when WordLink menu item is clicked.
- * @return  {html} 
+ * @return  {html}
  */
-export default function WordGroup({wordLinkDetails}) {
+export default function WordGroup({ wordLinkDetails }) {
   // wordLinkDetails = [{id: string, text: string, reference: useRef(null), onClick: function no args}, ...]
-  const WRDLNKS = wordLinkDetails.map( (menuItem, index) => 
+  const WRDLNKS = wordLinkDetails.map((menuItem, index) => (
     <WordLink
-      key={"word-group-link-"+index}
-      text= {menuItem.text}
+      key={"word-group-link-" + index}
+      text={menuItem.text}
       textref={menuItem.reference}
-      menuid= {menuItem.id}
-      onClick={() => menuItem.onClick()} 
+      menuid={menuItem.id}
+      onClick={() => menuItem.onClick()}
     />
-  );
+  ));
 
-  return (
-    <div className="wordlink-list">
-      {WRDLNKS}
-    </div>
-  );
+  return <div className="wordlink-list">{WRDLNKS}</div>;
 }
-
-{/* <WordLink
-        text="PROJECTS"
-        textref={props.references[0]}
-        menuid="menu0"
-        onClick={() => props.onClick("menu0", props.references[0])}
-      />
-      <WordLink
-        text="ABOUT"
-        textref={props.references[1]}
-        menuid="menu1"
-        onClick={() => props.onClick("menu1", props.references[1])}
-      />
-      <WordLink
-        text="CONTACT"
-        textref={props.references[2]}
-        menuid="menu2"
-        onClick={() => props.onClick("menu2", props.references[2])}
-      />
-      <WordLink
-        text="RESUME"
-        textref={props.references[3]}
-        menuid="menu3"
-        onClick={() => props.onClick("menu3", props.references[3])}
-      /> */}
